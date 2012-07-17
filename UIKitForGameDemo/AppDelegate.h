64df00ad7e76ceca94313f01b8ea929394e23bc5
shu223 <shuichi0526@gmail.com>
2012-07-17 15:03:14 +0900

    Initial Commit

---
+++ UIKitForGameDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UIKitForGameDemo
+//
+//  Created by Shuichi Tsutsumi on 12/07/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
